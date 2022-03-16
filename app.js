fd2d14ffa2afe6a82aa8b3410c05085c5a60d958
Sadman Sakib <ssakib4040@gmail.com>
2022-03-16 22:34:35 +0600

    process env PORT added in app.js

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
